00abc22c1e297a5c6e592851cdd7467e0fd6b27a
ArghSee <33350988+ArghSee@users.noreply.github.com>
2019-10-16 15:39:10 -0400

    Update openiparser.py
    initial commit

--- src/figtag/openiparser.py
+++ src/figtag/openiparser.py
@@ -1,2 +1,64 @@
 def openiparser(query: str, output_list: str):
-    pass
+    
+    import requests, string
+    
+    with open(output_list, 'w') as f:
+        f.write('uid\timg_path\tfig_num\tcaption')
+    
+    query_url_start = query.split('m=')[0]
+    query_url_end = query.split('n=100')[1]
+    
+    start = 1
+    end = 100
+
+    r = requests.get(query_url_start+'m='+str(start)+'&'+'n='+str(end)+query_url_end)
+    data = r.json()
+    
+    total = data['total']
+    
+    for result in data['list']:
+        
+        uid = ''.join(x for x in result['uid'] if x in string.printable)
+        imgLarge = ''.join(x for x in result['imgLarge'] if x in string.printable)
+        fid = ''.join(x for x in result['image']['id'] if x in string.printable)
+        capt = ''.join(x for x in result['image']['caption'] if x in string.printable)
+        
+        try:
+            with open(output_list, 'a') as f:
+                f.write('\n'+uid+'\t'+imgLarge+'\t'+fid+'\t'+capt)
+        except:
+            print(uid)
+            print(imgLarge)
+            print(fid)
+            print(capt)
+            raise
+            
+    if total > 100:
+        
+        for i in range(0, int((total-100)/100)+1):
+            
+            start = 101 + 100*i
+            end = 200 +100*i
+            
+            print(start, end)
+            
+            r = requests.get(query_url_start+'m='+str(start)+'&'+'n='+str(end)+query_url_end)
+            data = r.json()
+    
+            for result in data['list']:
+        
+                uid = ''.join(x for x in result['uid'] if x in string.printable)
+                imgLarge = ''.join(x for x in result['imgLarge'] if x in string.printable)
+                fid = ''.join(x for x in result['image']['id'] if x in string.printable)
+                capt = ''.join(x for x in result['image']['caption'] if x in string.printable)
+        
+                try:
+                    with open(output_list, 'a') as f:
+                        f.write('\n'+uid+'\t'+imgLarge+'\t'+fid+'\t'+capt)
+                except:
+                    print(uid)
+                    print(imgLarge)
+                    print(fid)
+                    print(capt)
+                    raise
+    
