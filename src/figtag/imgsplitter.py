a3615b13f84320b410c7681cb8211106f57b1a12
codeathoner <rvs.ncbi@gmail.com>
2019-10-17 11:04:12 -0400

    fix issues reported by `flake8`

--- src/figtag/imgsplitter.py
+++ src/figtag/imgsplitter.py
@@ -3,6 +3,7 @@ import numpy as np
 import requests
 import os
 
+
 def imgsplitter(image_url: str, image_uid: str, output_folder: str):
     req = requests.get(image_url)
     img = cv2.imdecode(np.asarray(bytearray(req.content), dtype="uint8"), cv2.IMREAD_COLOR)
@@ -16,14 +17,16 @@ def imgsplitter(image_url: str, image_uid: str, output_folder: str):
     if IsMultiPanel(hcuts, vcuts):
         Split(hcuts, vcuts, img, image_uid, output_folder)
     else:
-        print ("Not a multipanel")
+        print("Not a multipanel")
         cv2.imwrite("{}.png".format(os.path.join(output_folder, image_uid)), img)
+
 
 def _CutPoints(img, dim):
     row_means = cv2.reduce(img, dim, cv2.REDUCE_AVG, dtype=cv2.CV_32F).flatten()
     row_gaps = zero_runs(row_means)
-    row_cutpoints = (row_gaps[:,0] + row_gaps[:,1] - 1) / 2
+    row_cutpoints = (row_gaps[:, 0] + row_gaps[:, 1] - 1) / 2
     return [int(a) for a in row_cutpoints]
+
 
 def HorizontalCutPoints(img, dim=1):
     """
@@ -31,17 +34,20 @@ def HorizontalCutPoints(img, dim=1):
     """
     return _CutPoints(img, 1)
 
+
 def VerticalCutPoints(img):
     """
     Return available vertical cut points
     """
     return _CutPoints(img, 0)
 
+
 def IsMultiPanel(hcuts, vcuts):
     if hcuts or vcuts:
         return True
     else:
         return False
+
 
 def DeFrag(points, total_len):
     points.append(total_len)
@@ -62,10 +68,10 @@ def DeFrag(points, total_len):
 
 
 def Split(hcuts, vcuts, img, image_uid, output_folder):
-    index=0
+    index = 0
     height, width = img.shape[:2]
     DeFrag(hcuts, height)
-    DeFrag (vcuts, width)
+    DeFrag(vcuts, width)
 
     hcuts.append(height - 1)
     vcuts.append(width - 1)
@@ -89,13 +95,14 @@ def Split(hcuts, vcuts, img, image_uid, output_folder):
             index = index + 1
 
 # From https://stackoverflow.com/a/24892274/3962537
+
+
 def zero_runs(a):
     # A hack to remove noise from grayish background
-    a[np.where(a  < 0.5)] = 0
+    a[np.where(a < 0.5)] = 0
     # Create an array that is 1 where a is 0, and pad each end with an extra 0.
     iszero = np.concatenate(([0], np.equal(a, 0).view(np.int8), [0]))
     absdiff = np.abs(np.diff(iszero))
     # Runs start and end where absdiff is 1.
     ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
     return ranges
-
