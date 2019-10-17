8d85a9ead197e815b01f593804c4bd7a5459900f
codeathoner <rvs.ncbi@gmail.com>
2019-10-17 16:13:23 -0400

    fix line-length

--- src/figtag/indexer.py
+++ src/figtag/indexer.py
@@ -1,2 +1,3 @@
-def indexer(image_list: str, image_clusters: str, image_folder: str, raw_data: str, mesh_terms: str, database_file):
+def indexer(image_list: str, image_clusters: str, image_folder: str,
+            raw_data: str, mesh_terms: str, database_file):
     pass
