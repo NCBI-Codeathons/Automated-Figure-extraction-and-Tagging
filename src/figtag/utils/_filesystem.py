5552567cea440974a2de63d8016345aba19e0e65
codeathoner <rvs.ncbi@gmail.com>
2019-10-18 09:44:18 -0400

    add function to list files in a directory

--- src/figtag/utils/_filesystem.py
+++ src/figtag/utils/_filesystem.py
@@ -1,5 +1,7 @@
-from os import path
-from os import makedirs
+from os import makedirs, listdir, path
+import re
+from typing import List
+
 
 # for now
 Path = str
@@ -12,3 +14,19 @@ def create_folder(directory: Path) -> Path:
         raise ValueError(("Attempt to create {}, which already exists " +
                           "but is not a directory").format(directory))
     return directory
+
+
+def list_files(dir: Path, regexp: str = '.*') -> List[Path]:
+    if not path.isdir(dir):
+        raise Exception('Cannot list files: "' + dir + '" is not a ' +
+                        'directory or does not exist')
+
+    expr = re.compile(regexp, re.M)
+    files = []  # type: List[Path]
+    for file in listdir(dir):
+        file_path = path.join(dir, file)
+        if not path.isfile(file_path):
+            continue
+        if expr.search(file):
+            files.append(Path(file_path))
+    return files
