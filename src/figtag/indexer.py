be4cf2043c9c294a427e5f76bbe065423ab922fe
codeathoner <rvs.ncbi@gmail.com>
2019-10-18 11:20:29 -0400

    * incorporate Ryan Connor fixes * remove use of `os.cmd`

--- src/figtag/indexer.py
+++ src/figtag/indexer.py
@@ -1,54 +1,54 @@
+from figtag.utils._filesystem import list_files, Path
+
 import sqlite3
-import logging
 import pandas as pd
-import os
+from os import path
 
 
-_LOGGER = logging.getLogger('indexer')
+def indexer(openi_data: Path, image_folder: Path,
+            image_clusters: Path, mesh_terms: Path,
+            database_file: Path):
 
-
-def indexer(openi_data: str, image_folder: str,
-            image_clusters: str, mesh_terms: str,
-            database_file: str):
-    _LOGGER.info("Creating/Opening DB {}".format(database_file))
     db = database_file
     conn = sqlite3.connect(db)
 
-    _LOGGER.info("Saving OpenI data from {}".format(openi_data))
     raw = pd.read_csv(openi_data, '\t', header=0, index_col=0)
 
     raw.to_sql('users', con=conn, if_exists='replace')
 
-    cmd = 'ls ' + image_folder + ' > images.txt'
+    image_files = list_files(image_folder, r'.*\.png$')
+    image_list = [path.basename(img_path) for img_path in image_files]
 
-    os.system(cmd)
+    calcData = pd.DataFrame({'file_name': image_list})
 
-    _LOGGER.info("Saving image list from {}".format(image_folder))
-    calcData = pd.read_csv('images.txt',
-                           '\t',
-                           header=None).rename(columns={0: 'file_name'})
     calcData['idx'] = calcData.file_name.apply(lambda x: x.split('_')[1])
     calcData.idx = calcData.idx.astype(int)
     calcData['img_id'] = calcData.file_name.apply(lambda x: x.split('_')[-1].split('.')[-2])
-    calcData = calcData[['idx', 'img_id']].set_index('idx')
+    calcData.img_id = calcData.img_id.astype(int)
+    calcData = calcData[['idx', 'img_id']]
 
-    if image_clusters:
-        _LOGGER.info("Joining Image clusters data from {}".format(image_clusters))
-        calcData = _join_data(image_clusters, calcData)
+    clusters = pd.read_csv(image_clusters,
+                           '\t',
+                           header=None).rename(columns={0: 'path', 1: 'cid'})
+    clusters['idx'] = clusters.path.apply(lambda x: x.split('_')[1])
+    clusters['img_id'] = clusters.path.apply(lambda x: x.split('_')[-1].split('.')[-2])
+    clusters.idx = clusters.idx.astype(int)
+    clusters.img_id = clusters.img_id.astype(int)
+    clusters = clusters[['idx', 'img_id', 'cid']]
 
-    if mesh_terms:
-        _LOGGER.info("Joining MeSH terms from {}".format(mesh_terms))
-        calcData = _join_data(mesh_terms, calcData)
+    meshdf = pd.read_csv(mesh_terms,
+                         '\t',
+                         header=None).rename(columns={0: 'idx', 1: 'mesh'})
+    meshdf.idx = clusters.idx.astype(int)
+
+    calcData = calcData.join(meshdf.set_index('idx'),
+                             on='idx',
+                             how='left')
+    calcData = calcData.join(clusters.set_index(['idx', 'img_id']),
+                             on=['idx', 'img_id'],
+                             how='right')
 
     calcData.to_sql('calcData', con=conn, if_exists='replace')
 
     conn.commit()
     conn.close()
-
-
-def _join_data(data, calcData):
-    return calcData.join(pd.read_csv(data,
-                                     '\t',
-                                     header=0,
-                                     index_col=0),
-                         on='idx')
