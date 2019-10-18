3cd7b034997b7bc166a883316966b5606beba4e0
codeathoner <rvs.ncbi@gmail.com>
2019-10-18 09:46:23 -0400

    add calls to (mock) classifier

--- src/figtag/run.py
+++ src/figtag/run.py
@@ -1,9 +1,10 @@
-from figtag.classifier import classifier
+from figtag.classifier import _classify_image  # classifier
 from figtag.imgsplitter import imgsplitter, OPENI_URL
 # from figtag.indexer import indexer
 from figtag.openiparser import openiparser
 
-from figtag.utils._filesystem import create_folder, Path
+from figtag.utils._filesystem import (
+    create_folder, list_files, Path)
 
 import csv
 import logging
@@ -28,27 +29,41 @@ class _FigTagRunner():
         self.logger = logging.getLogger('_FigTagRunner')
 
     def run(self, query: str, model_path: str) -> int:
+        try:
+            return self._try_run(query, model_path)
+        except Exception as err:
+            print("An error occurred: " + str(err), file=sys.stderr)
+            return PROCESSING_FAILURE
+
+    def _try_run(self, query: str, model_path: str) -> int:
         self.logger.info('Running Open-I query')
-        openi_output_list = self._call_openiparser(query)
+        openi_output_list = self._parse_openi_query(query)
         if not openi_output_list:
             return PROCESSING_FAILURE
 
-        self.logger.info('Splitting images (if they are multipanel)')
+        self.logger.info('Processing images (multipanel ones will be split)')
         split_image_folder = self._split_images(openi_output_list)
         if not split_image_folder:
             return PROCESSING_FAILURE
         self.logger.info("Processed images are in folder %s", split_image_folder)
 
-        image_cluster_list = self._cluster_images(split_image_folder)
+        self.logger.info('Classifying images')
+        image_cluster_list = self._get_images_clusters(split_image_folder, model_path)
+        if not image_cluster_list:
+            return PROCESSING_FAILURE
+        self.logger.info("Information about images and the clusters they belong to "
+                         "can be found in '%s'", image_cluster_list)
 
+        self.logger.info('Indexing data')
         index_file = self._create_image_index(
             openi_output_list, image_cluster_list, split_image_folder)
-
+        if not index_file:
+            return PROCESSING_FAILURE
         print('Index file: {}'.format(index_file))
 
         return SUCCESS
 
-    def _call_openiparser(self, query: str) -> Path:
+    def _parse_openi_query(self, query: str) -> Path:
         if path.isfile(query) and path.exists(query):
             self.logger.info('Using existing file {}'.format(query))
             return query
@@ -56,39 +71,30 @@ class _FigTagRunner():
         openi_output_list: Path = path.join(self._output_folder, 'openi_output_list.tsv')
         self.logger.info('Saving Open-I results to {}'.format(openi_output_list))
 
-        try:
-            openiparser(query, openi_output_list)
-            return openi_output_list
-        except Exception as err:
-            print("An error occurred: " + str(err), file=sys.stderr)
-            return None
+        openiparser(query, openi_output_list)
+        return openi_output_list
 
     def _split_images(self, openi_output_list: Path) -> Path:
         img_output_folder: str = None
-        try:
-            with open(openi_output_list) as csvfile:
-                has_header = csv.Sniffer().has_header(csvfile.read(2048))
-                csvfile.seek(0)
-                data_reader = csv.reader(csvfile, delimiter='\t')
-                if has_header:
-                    next(data_reader, None)
+        with open(openi_output_list) as csvfile:
+            has_header = csv.Sniffer().has_header(csvfile.read(2048))
+            csvfile.seek(0)
+            data_reader = csv.reader(csvfile, delimiter='\t')
+            if has_header:
+                next(data_reader, None)
 
-                img_output_folder: Path = path.join(self._output_folder, 'images_split')
-                create_folder(img_output_folder)
+            img_output_folder: Path = path.join(self._output_folder, 'images_split')
+            create_folder(img_output_folder)
 
-                files_processed = 0
-                for row in data_reader:
-                    self._split_image(row, img_output_folder)
-                    if self._file_limit > 0:
-                        files_processed += 1
-                        if files_processed >= self._file_limit:
-                            self.logger.info(
-                                'Processed {} files'.format(files_processed))
-                            break
-
-        except Exception as err:
-            print("An error occurred: " + str(err), file=sys.stderr)
-            return None
+            files_processed = 0
+            for row in data_reader:
+                self._split_image(row, img_output_folder)
+                if self._file_limit > 0:
+                    files_processed += 1
+                    if files_processed >= self._file_limit:
+                        self.logger.info(
+                            'Processed {} files'.format(files_processed))
+                        break
 
         return img_output_folder
 
@@ -102,31 +108,40 @@ class _FigTagRunner():
         self.logger.info('Processing file {}'.format(full_url))
         imgsplitter(full_url, image_uid, img_output_folder)
 
-    def _cluster_images(self, split_image_folder: Path) -> Path:
-        classifier('image_path', 'model_path', 'output')
-        return '/dev/null'
+    def _get_images_clusters(self, split_image_folder: Path, model_path: str) -> Path:
+        # classifier('image_path', 'model_path', 'output')
+        image_files = list_files(split_image_folder, r'.*\.png$')
+
+        if not image_files:
+            self.logger.error("No PNG images found in '%s'", split_image_folder)
+            return None
+
+        image_cluster_list: Path = path.join(self._output_folder, 'image_cluster_list.tsv')
+
+        with open(image_cluster_list, 'wt') as imgclusterlst:
+            for image_path in image_files:
+                cluster_id = _classify_image(image_path, model_path)
+                imgclusterlst.write('{}\t{}\n'.format(
+                    image_path, cluster_id))
+
+        return image_cluster_list
+
+    def _get_image_cluster(self, image_path: Path) -> str:
+        return _classify_image(image_path, "model_path")
 
     def _create_image_index(self,
-                            image_list: Path, image_clusters: Path,
+                            openi_data: Path, image_clusters: Path,
                             image_folder: Path) -> Path:
-        # indexer('image_clusters', 'image_folder',
-        #         'raw_data', 'mesh_terms',
+        # indexer('openi_data', 'image_clusters', 'image_folder',
+        #         'mesh_terms',
         #         'database_file')
         return '/dev/null'
 
 
 def run(query: str, model_path: str, output_folder: str, file_limit: int = 0) -> int:
     """
-    Starts with parameters openi query, model file location, and output file
-    Call openiparser to get the list of tuples <img url,uid,caption,figid> and save to a file
-    Iterate over the images
-        call imgsplitter with a image url and uid and tell it where to store the output images
-    Iterate over the folder where `imgsplitter` saved the images
-        call classifier with an image file path, and get cluster id
-    Save image path, cluster id in a list
-    Call indexer with the list output by openiparser, the list of image, cluster id,
-            and the path to such images
-    Indexer creates an DB in a location provided as parameter
+    Takes an open-i query and a classification model file, and
+    produces an index file
     """
     try:
         figtagrunner = _FigTagRunner(output_folder, file_limit)
